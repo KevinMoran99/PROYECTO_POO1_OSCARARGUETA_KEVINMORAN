998fac3ef8a771e65fb0ddfb96de53aee55719cd
Kevin Morán García <krmorang@gmail.com>
2018-03-16 22:11:18 -0600

    Inserts provisionales en el script de la BD

--- src/main/resources/Catedra-POO.sql
+++ src/main/resources/Catedra-POO.sql
@@ -4,14 +4,14 @@ use pooC;
 
 create table user_types(
 id int not null primary key auto_increment,
-name varchar (100) not null
+name varchar (100) unique not null
 );
 
 create table users(
 id int primary key not null auto_increment,
 name varchar (100) not null,
 lastname varchar (100) not null,
-email varchar (255) not null,
+email varchar (255) unique not null,
 pass varchar (20) not null,
 user_type_id int not null,
 state bool not null default 1,
@@ -20,27 +20,27 @@ foreign key (user_type_id) references user_types(id)
 
 create table complaint_types(
 id int not null primary key auto_increment,
-name varchar (100) not null,
+name varchar (100) unique not null,
 taken_action enum('Remitir con autoridad competente', 'Tomar contacto con ISP y colegio') not null,
 state bool not null default 1
 );
 
 create table schools(
 id int not null primary key auto_increment,
-name varchar(100) not null,
+name varchar(100) unique not null,
 address varchar(255) not null,
 state bool not null default 1
 );
 
 create table authorities(
 id int not null primary key auto_increment,
-name varchar(100) not null,
+name varchar(100) unique not null,
 state bool not null default 1
 );
 
 create table providers(
 id int not null primary key auto_increment,
-name varchar(100) not null,
+name varchar(100) unique not null,
 state bool not null default 1
 );
 
@@ -75,13 +75,36 @@ foreign key (call_id) references calls(id),
 foreign key (provider_id) references providers(id)
 );
 
-
+-- INSERTS PROVISIONALES
 insert into user_types values (null, "Administrador");
 insert into user_types values (null, "Personal");
 
 insert into users values (null, "Oscar", "Méndez", "admin@gmail.com", "123", 1, 1);
 insert into users values (null, "Kevin", "Morán", "user@gmail.com", "123", 2, 1);
 
+insert into complaint_types values (null, "Pornografía Infantil", "Remitir con autoridad competente", 1);
+insert into complaint_types values (null, "Grooming (proposiciones sexuales a menores de edad)", "Remitir con autoridad competente", 1);
+insert into complaint_types values (null, "Anuncios con fines de explotación y turismo sexual infantil", "Remitir con autoridad competente", 1);
+insert into complaint_types values (null, "Acoso: Bullying o Cyberbullying", "Tomar contacto con ISP y colegio", 1);
+insert into complaint_types values (null, "Otros contenidos nocivos", "Tomar contacto con ISP y colegio", 1);
+
+insert into schools values (null, "Instituto Técnico Ricaldone", "Avenida Aguilares 218, San Salvador", 1);
+insert into schools values (null, "Colegio Salesiano Santa Cecilia", "Calle Don Bosco y Av. Manuel Gallardo, 1-1, Santa Tecla", 1);
+insert into schools values (null, "Colegio Don Bosco", "Soyapango", 1);
+insert into schools values (null, "Instituto Nacional General Francisco Menendez (INFRAMEN)", "20 Avenida Norte, El Salvador", 1);
+insert into schools values (null, "Escuela Americana", "San Salvador", 1);
+
+insert into authorities values (null, "Policía Nacional Civil", 1);
+insert into authorities values (null, "Procuraduría para la Defensa de los Derechos Humanos", 1);
+insert into authorities values (null, "Instituto Salvadoreño Para El Desarrollo Integral De La Niñez Y La Adolescencia", 1);
+insert into authorities values (null, "Asociación Nueva Vida Pro-Niñez y Juventud", 1);
+insert into authorities values (null, "Buró Federal de Investigaciones", 1);
+
+insert into providers values (null, "Facebook", 1);
+insert into providers values (null, "Google", 1);
+insert into providers values (null, "Twitter", 1);
+insert into providers values (null, "Youtube", 1);
+insert into providers values (null, "4chan", 1);
 
 
 
